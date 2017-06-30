5ea0997550d8a02c6daeaedd3f7d7aad3f2c5d10
Jaime Gonzalez Bonorino <jaimegbonorino@gmail.com>
2017-06-30 13:14:47 -0300

    Comenzado Chat 2

---
+++ SFMLVersion/Chat 2/mainCliente.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <SFML/Network.hpp>
+#define DATA_SIZE 100
+#define PUERTO 53000
+
+void mandarData(sf::TcpSocket * socket,std::string * mensaje){
+    const char * data = "";
+    data = mensaje->c_str();
+    if (socket->send(data, mensaje->size()) != sf::Socket::Done)
+    {
+       std::cout<<"Error enviando data.."<<std::endl;
+    }
+};
+void recibirData(sf::TcpSocket * socket){
+    char data[DATA_SIZE] = {0};
+    std::size_t bytesRecibidos;
+
+    if (socket->receive(data, DATA_SIZE, bytesRecibidos) != sf::Socket::Done)
+    {
+       std::cout<<"Error recibiendo data.."<<std::endl;
+    }
+    std::cout<<data<<" - "<<"recibido en "<<bytesRecibidos<<" bytes."<<std::endl;
+};
+
+void init(std::string * ip, std::string * name, std::string * mensaje){
+    std::cout<<"Ingrese la IP a la que quiere conectarse: "<<std::endl;
+    std::getline(std::cin,*ip,'\n');
+    std::cout<<"Ingrese su nombre: "<<std::endl;
+    std::getline(std::cin,*name,'\n');
+    name->append(": ");
+}
+
+void mensajear(sf::TcpSocket * socket, std::string *name){
+    std::string mensaje;
+    std::cout<<"Ingrese su mensaje: "<<std::endl;
+    std::getline(std::cin,mensaje,'\n');
+    mensaje.insert(0,*name);
+    mandarData(socket,&mensaje);
+}
+
+int main(int argc, char *argv[]){
+
+    std::string ip, name, msg;
+    std::string quit = "quit";
+    init(&ip,&name,&msg);
+    quit.insert(0,name);
+    sf::TcpSocket socket;
+    sf::IpAddress ipA(ip);
+    sf::SocketSelector socketSelector;
+
+    sf::Socket::Status status = socket.connect(ipA, PUERTO);
+    if (status != sf::Socket::Done)
+    {
+        std::cout<<"No se pudo conectar con server.."<<std::endl;
+    }else{
+        socketSelector.add(socket);
+        while(true){
+            if (socketSelector.wait(sf::seconds(2))){
+                if (socketSelector.isReady(socket)){
+                    recibirData(&socket);
+                }
+            }else{
+                //mensajear(&socket,&name);  
+            }
+        }
+    }
+    socket.disconnect();
+
+    return 0;
+}
---
+++ SFMLVersion/Chat 2/mainServer.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <SFML/Network.hpp>
+#define DATA_SIZE 100
+#define PUERTO 53000
+
+void mandarData(sf::TcpSocket * socket,std::string * mensaje){
+    const char * data = "";
+    data = mensaje->c_str();
+    if (socket->send(data, mensaje->size()) != sf::Socket::Done)
+    {
+       std::cout<<"Error enviando data.."<<std::endl;
+    }
+};
+
+void retransmitir(std::vector<sf::TcpSocket *> * socket,char * data,std::size_t bytes){
+    for(int i = 0; i < socket->size(); i++){
+        if (socket->at(i)->send(data,bytes) != sf::Socket::Done)
+        {
+        std::cout<<"Error enviando data.."<<std::endl;
+        }
+    }
+};
+
+void notificarUsuarios(std::vector<sf::TcpSocket *> * socket){
+    std::vector<const char *> data;
+    for(int i = 0; i < socket->size(); i++){
+        data.push_back(socket->at(i)->getRemoteAddress().toString().c_str());
+        std::cout<<"Cargando vector de direcciones de clientes..."<<std::endl;
+    }
+    for(int j = 0; j < data.size(); j++){
+        for(int i = 0; i < socket->size(); i++){
+            if (socket->at(i)->send(data.at(j),sizeof(data.at(j))) != sf::Socket::Done){
+                std::cout<<"Error enviando data.."<<std::endl;
+            }
+        }
+    }
+};
+
+
+void recibirData(sf::TcpSocket * socket, std::vector<sf::TcpSocket*> * sockets){
+    char data[DATA_SIZE] = {0};
+    std::size_t bytesRecibidos;
+
+    if (socket->receive(data, DATA_SIZE, bytesRecibidos) != sf::Socket::Done)
+    {
+       std::cout<<"Error recibiendo data.."<<std::endl;
+    }
+    std::cout<<data<<" - "<<"recibido en "<<bytesRecibidos<<" bytes."<<std::endl;
+    retransmitir(sockets,data,bytesRecibidos);
+};
+
+void aceptarConexiones(sf::TcpListener * listener,sf::SocketSelector * listenerSelector, std::vector<sf::TcpSocket*> * clientes,sf::SocketSelector * socketSelector){
+    sf::TcpSocket * socket = new sf::TcpSocket();
+    socket->setBlocking(false);
+    if (listener->accept(*socket) != sf::Socket::Done){
+            //std::cout<<"Error aceptando conexiones.."<<std::endl;
+    }else{
+            clientes->push_back(socket);
+            socketSelector->add(*socket);
+    }
+
+
+};
+void aceptarConexionPrimera(sf::TcpListener * listener, std::vector<sf::TcpSocket*> * clientes,sf::SocketSelector * socketSelector){
+    sf::TcpSocket * socket = new sf::TcpSocket();
+
+    if (listener->listen(PUERTO) != sf::Socket::Done){
+        std::cout<<"Error escuchando conexiones.."<<std::endl;
+    }else{
+        //Aceptando clientes
+        if (listener->accept(*socket) != sf::Socket::Done){
+            std::cout<<"Error aceptando conexiones.."<<std::endl;
+        }else{
+            clientes->push_back(socket);
+            socketSelector->add(*socket);
+            listener->setBlocking(false);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    std::vector<sf::TcpSocket*> clientes;
+    sf::TcpSocket cliente;
+    sf::IpAddress ip = sf::IpAddress::getLocalAddress();
+    sf::TcpListener listener;
+    unsigned short puerto = PUERTO;
+    sf::SocketSelector socketSelector;
+    sf::SocketSelector listenerSelector;
+    listenerSelector.add(listener);
+
+    aceptarConexionPrimera(&listener,&clientes,&socketSelector);
+
+    while(true){
+        if(socketSelector.wait(sf::seconds(2))){
+            for(int i = 0; i < clientes.size(); i++){
+                if(socketSelector.isReady(*clientes.at(i))){
+                    recibirData(clientes.at(i),&clientes);
+                }
+            }
+        }
+        else
+        {
+            //std::cout<<"No se recibio nada..."<<std::endl;
+            for(int i = 0; i < clientes.size(); i++){
+                notificarUsuarios(&clientes);
+            }
+            aceptarConexiones(&listener,&listenerSelector,&clientes,&socketSelector);
+        }
+    }
+
+    return 0;
+}
